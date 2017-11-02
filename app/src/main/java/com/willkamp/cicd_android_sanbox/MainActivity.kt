9ada714bbf2f32f28325043225b1fc16463afac0
William Kamp <manimaul@gmail.com>
2017-11-02 10:10:42 -0700

    initial commit

---
+++ app/src/main/java/com/willkamp/cicd_android_sanbox/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.willkamp.cicd_android_sanbox
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
